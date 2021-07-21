657b70279ed3eb0f9c6814406a852995456406a0
kgggh <68227318+kgggh@users.noreply.github.com>
2021-07-22 02:49:20 +0900

    spring-boot-msa 2일차
    복합마이크로서비스 추가
    전역 RestController 예외 핸들러 추가

--- some-temp-folder/api/src/main/java/se/magnus/api/core/product/ProductService.java
+++ some-temp-folder/api/src/main/java/se/magnus/api/core/product/ProductService.java
@@ -5,12 +5,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 public interface ProductService {
 
-    /**
-     * Sample usage: curl $HOST:$PORT/product/1
-     *
-     * @param productId
-     * @return the product, if found, else null
-     */
     @GetMapping(
         value    = "/product/{productId}",
         produces = "application/json")
